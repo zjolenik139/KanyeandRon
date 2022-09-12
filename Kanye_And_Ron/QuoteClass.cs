b7d57a0a813a5da07e80abe973745748cc133db7
Zachariah Olenik <zjolenik139@gmail.com>
2022-09-12 16:36:10 -0400

    Done

---
+++ Kanye_And_Ron/QuoteClass.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace KanyeAndRon
+{
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+
+            Console.WriteLine($"-----------");
+            Console.WriteLine($"Kanye: '{kanyeQuote}''");
+            Console.WriteLine($"-----------");
+
+        }
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"-----------");
+            Console.WriteLine($"Ron: '{ronQuote}'");
+            Console.WriteLine($"-----------");
+        }
+    }
+}
